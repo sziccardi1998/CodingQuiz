1a94540b33c0c9107aeafb553dda36c95197ef15
sziccardi1998 <sziccardifullstack@gmail.com>
2020-11-02 16:01:12 -0500

    Created score element and functions to handle scoring.

--- script.js
+++ script.js
@@ -1,15 +1,16 @@
 // create variables of all elements in document
 var timeEl = document.getElementById("time");
-
+var scoreEl = document.getElementById("score");
 //create timing and score variables
 var totalTime = 60;
 var timeSpent = 0;
 var score = 0;
 
-// create function to handle timing
-function timerStart() {
+// create function to handle timing and score board start up
+function gameStart() {
     setInterval(function() {
         timeEl.textContent = totalTime - timeSpent;
+        scoreEl.textContent = score;
         timeSpent++;
         if (timeSpent > totalTime){
             // something happens when time is up
@@ -17,4 +18,13 @@ function timerStart() {
     }, 1000)
 }
 
-timerStart();
+// create function to handle scoring
+function scoreUp() {
+    score++;
+}
+
+// start timer when game is started
+gameStart();
+
+// test scoring function
+scoreUp();
