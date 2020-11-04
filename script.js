b87a2be523690f2e01421b717d0b3062e35f6b76
sziccardi1998 <sziccardifullstack@gmail.com>
2020-11-04 10:46:54 -0500

    Created list construction and handling.

--- script.js
+++ script.js
@@ -2,7 +2,9 @@
 var timeEl = document.getElementById("time");
 var scoreEl = document.getElementById("score");
 var questionEl = document.getElementById("question");
-
+var responseEl = document.getElementById("response");
+var answerEl  = document.getElementById("answerList");
+var answerStore = [];
 // use test button to tune starting the game
 var startEl = document.getElementById("start");
 
@@ -11,6 +13,7 @@ var totalTime = 60;
 var timeSpent = 0;
 var score = 0;
 var questionIndex = 0;
+var correctIndex = 0;
 
 //create variable to house question text
 var questionText = "";
@@ -34,6 +37,22 @@ function welcomeScreen() {
     questionWrite();
 }
 
+// create function to clear answer options after question is answered correctly
+function answerRender() {
+  // clear answer options
+  answerEl.innerHTML = "";
+
+  // add new answers to the screen
+  for(var i = 0; i<answerStore.length(); i++){
+    var answer = answerStore[i];
+    var li = document.createElement("li");
+    li.textContent = answer;
+    li.setAttribute("data-index", i);
+    answerEl.appendChild(li);
+  }
+}
+
+
 // create function to handle scoring
 function scoreUp() {
   score++;
@@ -45,7 +64,7 @@ function correctSelection() {
   questionIndexIncrease();
 
   //display message showing answer was correct
-
+  responseEl.textContent = "Correct!";
 
   // increase the score
   scoreUp();
@@ -55,6 +74,7 @@ function correctSelection() {
 // function to handel incorrect answers
 function inocrrectSelection() {
   // display message that the selection was incorrect
+  responseEl.textContent = "Incorrect!";
 
   // remove time from the clock will chose amount later
   timeSpent = timeSpent + 2;
@@ -76,10 +96,25 @@ function firstQuestion() {
   questionText = "JavaScript is a type of ";
   questionWrite();
   questionIndexIncrease();
+  // write to buttons that will show possible answers
+  while(questionIndex === 1) {
+    // add event listners that are linked to incorrect and correct answer handling
+  }
+  // start the next function
 }
 
 document.addEventListener("click", function() {
     gameStart();
     questionText = "This is when we would load the first real question";
     questionWrite();
+})
+
+answerEl.addEventListener("click", function(event) {
+  var choice = event.target;
+  // if click on an answer check to see if it is the correct answer
+  if (choice.matches("li") === true) {
+    var answerSelection = choice.parentElement.getAttribute("data-index");
+  }
+
+
 })
